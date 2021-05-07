34376c68ce0168e25b72bd727c6e9587f6cd3488
DasCodes1441 <82446238+DasCodes1441@users.noreply.github.com>
2021-05-07 09:47:28 +0700

    Add files via upload

---
+++ Packle-v-0.2.sh
@@ -0,0 +1,24 @@
+clear
+echo ""
+echo "WELCOME TO Packle"
+echo "enter an option. helpme for help"
+read VAR
+#above is the first code that get executed when you execute Packle.this will control where are you going or can be called intro part
+if [[ $VAR -eq "helpme" ]];
+then
+  read VAR2
+  if [[$VAR2 -eq about this program]];
+   echo "Packle (PACKage bundLE)is an application to install basic packages you need alongside the application you wanna use.example dev PACKage bundLE will install the programming language of the choices an will add a few more program to ensure you have a pleasure experience doing whatever it is"
+  echo "type exit to exit"
+  echo "type start on the next one to start"
+  read VAR4
+#above is the code to help you when you are a complete beginner on Packle"
+if [[$VAR4 -eq start]]
+
+if [[$VAR -eq start]]
+  read VAR3
+  if [[$VAR3 -eq exit]];
+   clear
+   echo 
+  fi
+    fi
